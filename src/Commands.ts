2107606f190ef9d5b362bc92b1b1e2810bfadc25
Moonpile <3959760+Moonpile@users.noreply.github.com>
2022-09-02 00:19:17 -0400

    added /phase command

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,4 +1,5 @@
 import { Command } from "./Command";
+import { Phase } from "./commands/Phase";
 import { Roll } from "./commands/Roll";
 
-export const Commands: Command[] = [Roll];
+export const Commands: Command[] = [Roll, Phase];
---
+++ src/commands/Phase.ts
@@ -0,0 +1,45 @@
+import {
+  ApplicationCommandOptionData,
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+  ChatInputCommandInteraction,
+  Client,
+  Options,
+} from "discord.js";
+import { Dice } from "../dice/Dice";
+import { Command } from "../Command";
+
+export const Phase: Command = {
+  name: "phase",
+  description: `Gives a random phase of the moon.`,
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+      type: ApplicationCommandOptionType.Integer,
+      name: "phasenum",
+      description: `A number from 1 to 8 corresponding to a phase of the moon.`,
+      required: false,
+    },
+  ],
+  run: async (client: Client, interaction: ChatInputCommandInteraction) => {
+
+    let phasenum = interaction.options.getInteger("phasenum") ;
+
+    if (!phasenum) {
+    const dice = new Dice("1d8");
+    dice.roll();
+      phasenum = dice.total || 2; 
+    }
+
+    const phases = ["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘"];
+    const phaseNames = ["New", "Waxing Crescent", "First Quarter", "Waxing Gibbous", "Full", "Waning Gibbous", "Last Quarter", "Waning Crescent"]
+
+
+    const content = `${phases[phasenum]}  ${phaseNames[phasenum]}`;
+
+    await interaction.followUp({
+      ephemeral: true,
+      content: content,
+    });
+  },
+};
