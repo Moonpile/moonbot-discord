0a38e4bd6d3953098a41d9c4b9b04d3c6adfd3cd
Moonpile <3959760+Moonpile@users.noreply.github.com>
2022-09-01 21:01:40 -0400

    Delete commands before running (and re-creating commands)

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,25 +1,45 @@
-import * as dotenv from 'dotenv'; 
-import { Client, GatewayIntentBits } from "discord.js";
-import interactionCreate from './listeners/interactionCreate';
+import * as dotenv from "dotenv";
+import { Client, GatewayIntentBits, REST, Routes } from "discord.js";
+import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
 
 dotenv.config();
 
-console.log("Bot is starting...");
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+const applicationId = process.env.APPLICATION_ID;
 
-console.log(process.env.TOKEN);
+if (!token || !clientId || !guildId || !applicationId) {
+  console.error("Missing required environment variable.");
+} else {
+  // Delete all commands, they will be re-created
+  const rest = new REST({ version: "10" }).setToken(token);
+  rest
+    .put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+    .then(() => console.log("Successfully deleted all guild commands."))
+    .catch(console.error);
+  rest
+    .put(Routes.applicationCommands(clientId), { body: [] })
+    .then(() => console.log("Successfully deleted all application commands."))
+    .catch(console.error);
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
-});
+  console.log("Bot is starting...");
 
-// This actually sets the status to online
-client.once("ready", () => {
-  console.log("Ready!");
-});
+  console.log(token);
 
-ready(client);
-interactionCreate(client);
-client.login(process.env.TOKEN);
+  const client = new Client({
+    intents: [GatewayIntentBits.Guilds],
+  });
 
-// console.log(client);
+  // This actually sets the status to online
+  client.once("ready", () => {
+    console.log("Ready!");
+  });
+
+  ready(client);
+  interactionCreate(client);
+  client.login(token);
+
+  // console.log(client);
+}
--- src/commands/Roll.ts
+++ src/commands/Roll.ts
@@ -1,26 +1,39 @@
-import { ApplicationCommandType, ChatInputCommandInteraction, Client, CommandInteraction } from "discord.js";
+import {
+  ApplicationCommandOptionData,
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+  ChatInputCommandInteraction,
+  Client,
+  Options,
+} from "discord.js";
 import { Dice } from "../dice/Dice";
 import { Command } from "../Command";
 
-
 export const Roll: Command = {
-    name: "r",
-    description: "Returns a greeting",
-    type: ApplicationCommandType.ChatInput,
-    run: async (client: Client, interaction: ChatInputCommandInteraction) => {
-    
-        const diceExpression = interaction.options.getString("diceexpression") || "1d100"
+  name: "r",
+  description: `Rolls dice based on the provided dice expression, ie "1d100", "3d6", "4d6l1".`,
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+      type: ApplicationCommandOptionType.String,
+      name: "diceexpression",
+      description: `The dice to roll, ie "3d6", "1d100", or "4d6l1"`,
+      required: true,
+    },
+  ],
+  run: async (client: Client, interaction: ChatInputCommandInteraction) => {
+    const diceExpression =
+      interaction.options.getString("diceexpression") || "1d100";
 
+    const dice = new Dice(diceExpression);
 
-        const dice = new Dice(diceExpression);
+    dice.roll();
 
-        dice.roll();
+    const content = `${diceExpression} = ${dice.total}      ${dice.format}`;
 
-        const content = `${diceExpression} = ${dice.total}      ${dice.format}`;
-
-        await interaction.followUp({
-            ephemeral: true,
-            content: content
-        });
-    }
+    await interaction.followUp({
+      ephemeral: true,
+      content: content,
+    });
+  },
 };
